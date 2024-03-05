a1fa79682d45445ba99c7ad95d801bad836ba3ea
Troy <yortozne@gmail.com>
2024-03-05 15:49:04 +0800

    added componets

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import Image from "next/image";
 import Background from "@/components/Background"
 import Overlay from "@/components/Overlay"
+import Features from "@/components/Features"
+import Products from "@/components/Products"
+import About from "@/components/About"
 
 export default function Home() {
   return (
     <>
     <Background/>
     <Overlay/>
+    <About/>
+    <Features/>
+    <Products/>
     </>
   );
 }
---
+++ components/About.jsx
@@ -0,0 +1,83 @@
+'use client'
+
+import {
+  Container,
+  SimpleGrid,
+  Image,
+  Flex,
+  Heading,
+  Text,
+  Stack,
+  StackDivider,
+  Icon,
+  useColorModeValue,
+} from '@chakra-ui/react'
+import { ReactElement } from 'react'
+
+const Feature = ({ text, icon, iconBg }) => {
+  return (
+    <Stack direction={'row'} align={'center'}>
+      <Flex w={8} h={8} align={'center'} justify={'center'} rounded={'full'} bg={iconBg}>
+        {icon}
+      </Flex>
+      <Text fontFamily={'inter'} fontWeight={600}>{text}</Text>
+    </Stack>
+  )
+}
+
+export default function SplitWithImage() {
+  return (
+    <Container maxW={'5xl'} py={12}>
+      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+        <Stack spacing={4}>
+          {/* <Text
+            textTransform={'uppercase'}
+            color={'blue.400'}
+            fontWeight={600}
+            fontSize={'sm'}
+            bg={useColorModeValue('blue.50', 'blue.900')}
+            p={2}
+            alignSelf={'flex-start'}
+            rounded={'md'}>
+            About Us
+          </Text> */}
+          <Heading fontFamily={'inter'} >About</Heading>
+          <Text color={'gray.500'} fontSize={'lg'}>
+          We are dedicated to bringing healthier and quality produce to people in the local space. 
+          </Text>
+          <Stack
+            spacing={4}
+            divider={
+              <StackDivider borderColor={useColorModeValue('gray.100', 'gray.700')} />
+            }>
+            <Feature
+            
+              iconBg={useColorModeValue('yellow.100', 'yellow.900')}
+              text={'Business Planning'}
+            />
+            <Feature
+              
+              iconBg={useColorModeValue('green.100', 'green.900')}
+              text={'Financial Planning'}
+            />
+            <Feature
+
+              iconBg={useColorModeValue('purple.100', 'purple.900')}
+              text={'Market Analysis'}
+            />
+          </Stack>
+        </Stack>
+        <Flex>
+          <Image
+            rounded={'md'}
+            alt={'feature image'}
+            src={
+              'https://images.unsplash.com/photo-1554200876-56c2f25224fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+            }
+            objectFit={'cover'}
+          />
+        </Flex>
+      </SimpleGrid>
+    </Container>
+  )
+}
