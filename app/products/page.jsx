cdda517befbddcc634677b94e08bb3502f68f666
Troy <yortozne@gmail.com>
2024-05-23 20:15:44 +0800

    removed some dependencies

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,8 +1,8 @@
 
-import Products from "@/components/Products"
-import FoodProducts from "@/components/FoodProducts"
-import DrinkProducts from "@/components/DrinkProducts"
-import AthleticProducts from "@/components/AthleticProducts"
+import Products from "./components/Products"
+import FoodProducts from "./components/FoodProducts"
+import DrinkProducts from "./components/DrinkProducts"
+import AthleticProducts from "./components/AthleticProducts"
 
 export default function Home() {
   return (
